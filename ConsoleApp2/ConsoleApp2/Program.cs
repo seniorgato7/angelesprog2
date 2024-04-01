6a663d8f59579c2c91fed9c9df4b54a4d93dac69
Josheldon Angeles <164700685+seniorgato7@users.noreply.github.com>
2024-04-02 03:56:29 +0800

    Initial commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int num = 1; num <= 100; num++)
+            {
+                if (num % 15 == 0) // Check for divisibility by both 3 and 5 first
+                {
+                    Console.WriteLine("Hello Goodbye");
+                }
+                else if (num % 3 == 0)
+                {
+                    Console.WriteLine("Hello");
+                }
+                else if (num % 5 == 0)
+                {
+                    Console.WriteLine("Goodbye");
+                }
+                else
+                {
+                    Console.WriteLine(num);
+                }
+            }
+        }
+    }
+}
